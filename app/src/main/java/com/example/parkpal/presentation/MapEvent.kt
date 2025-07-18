29fe81b664bddb8543ca133579fb1589a421fdf7
Benslimane Taha <taha.benslimane@etu.umontpellier.fr>
2025-07-18 13:05:26 +0200

    Deleted duplicate files (the originals in util folder)

--- app/src/main/java/com/example/parkpal/presentation/MapEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.example.parkpal.presentation
-
-import com.example.parkpal.domain.model.ParkingLocation
-
-sealed class MapEvent {
-    data class OnParkingLocationClicked(val parkingLocation: ParkingLocation) : MapEvent()
-    data class OnParkMyCarClicked(val parkingLocation: ParkingLocation) : MapEvent()
-}
